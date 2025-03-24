c376857939f53a80e311412384cb67759bdaf7ed
Yiroma <yiromaric@gmail.com>
2025-03-24 10:10:18 +0100

    server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+/** Création du serveur */
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 /** Fonction auto appellée (évite la mise en constante) permettant de lancer le serveur */
 (async () => {
   const { url } = await startStandaloneServer(server, {
